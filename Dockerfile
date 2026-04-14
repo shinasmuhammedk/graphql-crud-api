7e11ecafcd7129f3d542d64e5f336f0f9ee67e85
shinaskvzr-cell <shinaskvzr@gmail.com>
2026-04-14 10:46:28 +0530

    added docker automation

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,14 @@
-# Use official Golang image
-FROM golang:1.22-alpine
+FROM golang:1.25-alpine
 
-# Set working directory
 WORKDIR /app
 
-# Copy go mod files first (for caching)
 COPY go.mod go.sum ./
-RUN go mod tidy
+RUN go mod download
 
-# Copy project files
 COPY . .
 
-# Build the app
 RUN go build -o main .
 
-# Expose port
 EXPOSE 8080
 
-# Run the app
 CMD ["./main"]
--- db/mongo.go
+++ db/mongo.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/v2/mongo"
@@ -11,23 +12,29 @@ import (
 
 var Client *mongo.Client
 
-func ConnectDB(){
-    
-    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    defer cancel()
-    
-    client, err := mongo.Connect(
-        options.Client().ApplyURI("mongodb://localhost:27017"),
-    )
-    if err != nil{
-        log.Fatal(err)
-    }
-    
-    err = client.Ping(ctx, nil)
-    if err != nil{
-        log.Fatal("MongoDB not connected")
-    }
-    
-    Client = client
-    log.Println("MongoDB connected")
+func ConnectDB() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// ✅ Get Mongo URI from environment
+	uri := os.Getenv("MONGO_URI")
+	if uri == "" {
+		uri = "mongodb://localhost:27017" // fallback for local
+	}
+
+	client, err := mongo.Connect(
+		options.Client().ApplyURI(uri),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("MongoDB not connected")
+	}
+
+	Client = client
+	log.Println("MongoDB connected")
 }
